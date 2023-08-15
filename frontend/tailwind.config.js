fc1d5832db7b6267544fbe50c08f255a14cd5f4b
jeffubayi <jeffubayi@gmail.com>
2023-08-15 13:37:23 +0300

    setup tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,49 @@
+const plugin = require("tailwindcss/plugin");
+
+const myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".backface-visible": {
+      "backface-visibility": "visible",
+    },
+    ".backface-hidden": {
+      "backface-visibility": "hidden",
+    },
+    ".rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+  });
+});
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+      },
+      animation: {
+        fadeOut: "fadeOut 1s ease-out",
+        fadeIn: "fadeIn .4s ease-in",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "50%": { opacity: ".5" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "50%": { opacity: ".5" },
+          "100%": { opacity: "0" },
+        },
+      },
+    },
+  },
+  plugins: [myclass],
+};
